ae8990360c19dba5999933f3ae2e79d780fdfd13
Fadi Shawki <fadiorbitmines@gmail.com>
2023-12-07 22:02:51 +0100

    2023/12/07

--- src/routes/papers/2023.OnOrbits.tsx
+++ src/routes/papers/2023.OnOrbits.tsx
@@ -13,6 +13,7 @@ import Arc from "../../lib/paper/layout/Arc";
 import TODO from "../../lib/paper/layout/TODO";
 import Link from "../../lib/paper/layout/Link";
 import {renderable} from "../../lib/typescript/React";
+import {Intent, Tag} from "@blueprintjs/core";
 
 const OnOrbits = () => {
   const navigate = useNavigate();
@@ -64,71 +65,13 @@ const OnOrbits = () => {
 
       </Section>
     </Arc>
-    <Arc head="Arc: Interface">
-
-    </Arc>
-    <Arc head="Arc: Category Theory">
-      <Link link="https://github.com/orbitmines/category-theory" icon={ORGANIZATIONS.github.key}/>
-
-    </Arc>
-    <Arc head="Arc: Homotopy Type Theory">
-      <Link link="https://github.com/orbitmines/homotopy-type-theory" icon={ORGANIZATIONS.github.key}/>
-
-    </Arc>
-    <Arc head="Arc: ZX-Calculus">
-
-    </Arc>
-    <Arc head="Arc: WebAssembly">
-      <Link link="https://github.com/orbitmines/wasm" icon={ORGANIZATIONS.github.key}/>
-
-      <Section head="Why WebAssembly?" sub="A broader perspective">
-
-      </Section>
-
-      <Section head={<>
-        <Reference
-          index={referenceCounter()}
-          reference={{
-            title: "LEB128",
-            link: "https://en.wikipedia.org/wiki/LEB128"
-          }}
-          simple inline
-        />
-      </>} sub="Little Endian Base 128 encoding">
-        <TODO> </TODO>
-      </Section>
-
-      <Section head={<>
-        <Reference index={referenceCounter()} reference={{title: "Unicode", link: "https://www.unicode.org/versions/latest/"}} simple inline />
-      </>} sub="UTF-8 encoding">
-        {/*Before getting further complexity in, it will probably help to construct the interface by which characters will be used, so we can use more familiar text-like symbols in the structures. For historical reasons and the choice of WebAssembly, I'll initially use UTF-8 for this. Just to simplify, I'll for now ignore the visualization of the Unicode scalar values.*/}
-
-        {/*<BR/>*/}
-
-        <TODO> </TODO>
-      </Section>
-
-      <Section head={<>
-        <Reference
-          simple
-          inline
-          index={referenceCounter()}
-          reference={{
-            title: "IEEE 754",
-            link: "https://ieeexplore.ieee.org/document/8766229",
-            published: [{name: 'IEEE Std 754-2019 (Revision of IEEE 754-2008)'} as any],
-            year: "2019",
-            pointer: "1-84"
-          }}
-        />
-      </>} sub="Floating-Point Arithmetic">
-        <TODO> </TODO>
-      </Section>
-    </Arc>
-    <Arc head="Arc: Timeline">
-      <Section head="A quick journey of getting here">
-
-      </Section>
+    <Arc head="Arc: OrbitMines Explorer: The Project">
+      <Link
+          link="https://github.com/orbitmines/orbitmines.com/pull/1"
+          name={<span>
+        OrbitMines Explorer - <Tag intent={Intent.WARNING} minimal multiline style={{fontSize: '1rem', paddingTop: '0px', paddingBottom: '0px'}}>WIP</Tag> Preliminary Technical Implementation/Exploration
+      </span>}
+          icon={ORGANIZATIONS.github.key} />
     </Arc>
 
     <Arc head="Wrapping up">
@@ -143,7 +86,25 @@ const OnOrbits = () => {
 
         I might also fail to mention relevant works (or personal interactions) which have either had impact on my line of thinking more non-trivially, or whose ideas aren't mentioned here, or in my other writing, directly. My current practical approach to this has therefore been to publish many kinds of information I expose myself to (during the time I'm working on these projects). Which hopefully support the more explicit referencing done here. I hope to further expand on this concept in future works.
       </Section>
+      <Section head="On language and my bending of it">
+        In my - albeit limited - number of radians around our particular sun, I have found that I myself am (and likely by extension others are) quite inconsistent. Quite regularly I would like to rephrase certain (historical) expressions, dissatisfied by their vagueness or inaccuracy. Or to make the conceptual leap and guess that the only way my memory is even slightly functional is the apparent ability to infer or remember by resolving pointers from context somewhat consistently. A form of permeating uncertain caution seems appropriate when thinking through these abstract concepts: This could be interpreted as a reference carried forward in time with the potential to modify any particular assumption.
+
+        <BR/>
+
+        Additionally, I'm quite certain that my use of language contains a style which is unusual. Likely containing seemingly duplicated word sequences, possibly overly cautious /or not at all, and quite certainly seemingly (possibly on purpose) contradictory in many instances.
+
+        <BR/>
+
+        In this sense, I don't necessarily care about the accuracy of any particular sentence. Whether in this language, or one it's translated to. I care whether the more abstract pattern of ideas which I'm trying to communicate is sufficiently transferred through that language. And the ability to - through time, through correction -, adjust the ideas as deemed by me/you as particularly inconsistent to warrant its change.
+
+      </Section>
     </Arc>
+    <Arc head="Arc: OrbitMines' Timeline (2013-2023)">
+      <Section head="A quick journey of getting here">
+
+      </Section>
+    </Arc>
+
   </Paper>;
 }
 
