745b33591eccdab89ef3a68e63935ac5c0e88820
Fadi Shawki <fadi.shawki@orbitmines.com>
2025-09-07 14:19:55 +0200

    NGI Grant Proposal #3

--- orbitmines.com/src/routes/Archive.tsx
+++ orbitmines.com/src/routes/Archive.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 import {useParams} from "react-router-dom";
 import Error from "./Error";
 import _2024_02_NGI_GrantProposal from "./archive/2024.02.NGI.GrantProposal";
+import _2025_09_NGI_GrantProposal from "./archive/2025.09.NGI.GrantProposal3";
 import _2024_02_OrbitMines_as_a_Game_Project from "./archive/2024.02.OrbitMines_as_a_Game_Project";
 
 const ITEMS: { [key: string]: any } = {
+    '2025-09-ngi-grant-proposal': _2025_09_NGI_GrantProposal,
     '2024-02-ngi-grant-proposal': _2024_02_NGI_GrantProposal,
     '2024-02-orbitmines-as-a-game-project': _2024_02_OrbitMines_as_a_Game_Project
 }
